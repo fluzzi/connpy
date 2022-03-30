fd8b367d52aa6bc2cbb45b5f151766643f5082ac
fluzzi <fluzzi@gmail.com>
2022-03-30 19:51:54 -0300

    adding threading nodes automation

--- conn/__init__.py
+++ conn/__init__.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
-from .core import node
+from .core import node,nodes
 from .configfile import configfile
 from .connapp import connapp
 
 __version__ = "2.0"
-__all__ = ["node", "configfile", "connapp"]
+__all__ = ["node", "nodes", "configfile", "connapp"]
