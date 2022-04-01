7fb4ea8b8ca8cf7c423a62d03696ec8fc1e121da
fluzzi <fluzzi@gmail.com>
2022-04-01 17:53:51 -0300

    change setup and versions

--- conn/__init__.py
+++ conn/__init__.py
@@ -3,6 +3,8 @@
 from .core import node,nodes
 from .configfile import configfile
 from .connapp import connapp
+from pkg_resources import get_distribution
 
-__version__ = "2.0"
 __all__ = ["node", "nodes", "configfile", "connapp"]
+__version__ = "2.0.9"
+__author__ = "Federico Luzzi"
