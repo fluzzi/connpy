5c9c605184dcec8549b1f67a835f890a62f938cf
fluzzi <fluzzi@gmail.com>
2022-04-04 19:09:00 -0300

    added completion bash/zsh

--- connpy/__init__.py
+++ connpy/__init__.py
@@ -126,7 +126,7 @@ from .connapp import connapp
 from pkg_resources import get_distribution
 
 __all__ = ["node", "nodes", "configfile", "connapp"]
-__version__ = "2.0.0"
+__version__ = "2.0.1"
 __author__ = "Federico Luzzi"
 __pdoc__ = {
     'core': False,
